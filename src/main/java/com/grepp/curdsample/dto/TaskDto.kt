6780dcb1c9257eebde1c68d9fab129bbb5d54058
eddie1031 <archaco123@gmail.com>
2025-06-03 10:41:40 +0900

    TaskDto 코틀린 변환 1

--- src/main/java/com/grepp/curdsample/dto/TaskDto.kt
+++ src/main/java/com/grepp/curdsample/dto/TaskDto.kt
@@ -10,29 +10,34 @@ import lombok.AllArgsConstructor
 import lombok.Data
 import lombok.NoArgsConstructor
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class TaskDto {
-    private var code: String? = null
+data class TaskDto(
+    var code: String? = null,
 
-    private var title: @NotBlank(message = "작업 이름은 반드시 입력되어야 합니다") String? = null
+    @field: NotBlank(message = "작업 이름은 반드시 입력되어야 합니다")
+    var title:  String? = null,
 
-    private var description: String? = null
+    var description: String? = null,
 
-    private var priority: @Min(value = 0) Int? = null
+    @field: Min(value = 0)
+    var priority:  Int? = null,
 
-    private var completeStatus = false
+    var completeStatus: Boolean = false,
 
-    private var startTime: @NotBlank(message = "날짜는 반드시 들어있어야 합니다") @Pattern(
+    @field: Pattern(
         regexp = "^(0[1-9]|1[0-2])/([0-2][0-9]|3[01])/\\d{4}$",
         message = "올바른 날짜를 입력하여 주시기 바랍니다."
-    ) String? = null
+    )
+    @field: NotBlank(message = "날짜는 반드시 들어있어야 합니다")
+    var startTime:   String? = null,
 
-    private var endTime: @NotBlank(message = "날짜는 반드시 들어있어야 합니다") @Pattern(
+    @field: Pattern(
         regexp = "^(0[1-9]|1[0-2])/([0-2][0-9]|3[01])/\\d{4}$",
         message = "올바른 날짜를 입력하여 주시기 바랍니다."
-    ) String? = null
+    )
+    @field: NotBlank(message = "날짜는 반드시 들어있어야 합니다")
+    var endTime:   String? = null
+
+) {
 
     val priorityLevel: String
         get() = PriorityResolver.resolve(priority)
