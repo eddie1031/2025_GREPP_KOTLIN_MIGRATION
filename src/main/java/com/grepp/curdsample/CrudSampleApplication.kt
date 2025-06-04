dc92b94e670b20e87c899ea8439f1eb8d95679d0
eddie1031 <archaco123@gmail.com>
2025-06-04 10:21:02 +0900

    SpringBootApplication Kotlin으로 전환

---
+++ src/main/java/com/grepp/curdsample/CrudSampleApplication.kt
@@ -0,0 +1,16 @@
+package com.grepp.curdsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class CrudSampleApplication
+
+
+fun main(args: Array<String>) {
+    runApplication<CrudSampleApplication>(*args)
+}
+
+
+
